25002abc05cc7fa2716c2c77b4f39f6a7999a7ab
ThatDraenGuy <haykin.oleg@rambler.ru>
2025-11-27 19:51:06 +0300

    task 1 started

--- src/Part1/Tasks.hs
+++ src/Part1/Tasks.hs
@@ -2,27 +2,55 @@ module Part1.Tasks where
 
 import Util(notImplementedYet)
 
+fact :: Integer -> Integer
+fact i = if i <= 1 then 1 else i * fact (i - 1)
+
+taylorAtZero x stepFunc currSum n eps =
+    let sum = stepFunc n x in
+    if abs sum < eps
+    then currSum 
+    else taylorAtZero x stepFunc (currSum + sum) (n + 1) eps
+
+sinStepFunc n x = 
+    let num = 2 * n + 1
+        sign = if (mod n 2) == 0 then 1 else -1
+    in sign * x ** (fromInteger num) / (fromInteger . fact $ num)
+
+mySin' x = taylorAtZero x sinStepFunc 0 0 0.0001
+
+cosStepFunc n x =
+    let num = 2 * n
+        sign = if (mod n 2) == 0 then 1 else -1
+    in sign * x ** (fromInteger num) / (fromInteger . fact $ num)
+
+myCos' x = taylorAtZero x cosStepFunc 0 0 0.0001
+
 -- синус числа (формула Тейлора)
 mySin :: Double -> Double
-mySin = notImplementedYet
+mySin x = let adjusted = x - 2 * pi * (fromInteger . floor $ (x / (2 * pi))) in
+    mySin' adjusted
 
 -- косинус числа (формула Тейлора)
 myCos :: Double -> Double
-myCos = notImplementedYet
+myCos x = let adjusted = x - 2 * pi * (fromInteger . floor $ (x / (2 * pi))) in
+    myCos' adjusted
 
 -- наибольший общий делитель двух чисел
 myGCD :: Integer -> Integer -> Integer
-myGCD = notImplementedYet
+myGCD a 0 = abs a
+myGCD a b = gcd (abs b) (mod (abs a) (abs b))
 
 -- является ли дата корректной с учётом количества дней в месяце и
 -- вискокосных годов?
 isDateCorrect :: Integer -> Integer -> Integer -> Bool
-isDateCorrect = notImplementedYet
+isDateCorrect d m y = notImplementedYet
 
 -- возведение числа в степень, duh
 -- готовые функции и плавающую арифметику использовать нельзя
 myPow :: Integer -> Integer -> Integer
-myPow = notImplementedYet
+myPow _ 0 = 1
+myPow a 1 = a
+myPow a b = a * myPow a (b - 1)
 
 -- является ли данное число простым?
 isPrime :: Integer -> Bool
