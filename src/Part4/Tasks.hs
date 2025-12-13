3a84a975e31928fd624077f9b7786bceb7199609
ThatDraenGuy <haykin.oleg@rambler.ru>
2025-12-13 16:49:31 +0300

    tasks 4+5 done

--- src/Part4/Tasks.hs
+++ src/Part4/Tasks.hs
@@ -16,17 +16,45 @@ rlistToList lst =
 
 -- Реализуйте обратное преобразование
 listToRlist :: [a] -> ReverseList a
-listToRlist = notImplementedYet
+listToRlist lst = 
+    reversed (reverse lst)
+    where reversed [] = REmpty
+          reversed (h:t) = (reversed t) :< h
 
 -- Реализуйте все представленные ниже классы (см. тесты)
-instance Show (ReverseList a) where
-    showsPrec = notImplementedYet
-    show = notImplementedYet
-instance Eq (ReverseList a) where
-    (==) = notImplementedYet
-    (/=) = notImplementedYet
+instance (Show a) => Show (ReverseList a) where
+    showsPrec p lst = showChar '[' . show' lst . showChar ']'
+        where show' REmpty = showString ""
+              show' (REmpty :< t) = showsPrec p t
+              show' (h :< t) = show' h . showChar ',' . showsPrec p t
+
+instance (Eq a) => Eq (ReverseList a) where
+    (==) REmpty REmpty = True
+    (==) REmpty _ = False
+    (==) _ REmpty = False
+    (==) (h1 :< t1) (h2 :< t2) = if t1 == t2 then h1 == h2 else False
+    (/=) a b = not (a == b)
+
 instance Semigroup (ReverseList a) where
+    (<>) a (REmpty) = a
+    (<>) a (REmpty :< t) = a :< t
+    (<>) a (h :< t) = (a <> h) :< t
+
 instance Monoid (ReverseList a) where
+    mempty = REmpty
+    mappend a b = a <> b
+
 instance Functor ReverseList where
+    fmap f REmpty = REmpty
+    fmap f (h :< t) = (fmap f h) :< (f t)
+
 instance Applicative ReverseList where
+    pure a = REmpty :< a
+    (<*>) REmpty _ = REmpty
+    (<*>) _ REmpty = REmpty
+    (<*>) (h1 :< t1) (h2 :< t2) = (h1 <*> h2) <> (fmap t1 h2) <> (fmap (\x -> x t2) h1) :< (t1 t2)
+
 instance Monad ReverseList where
+    (>>=) lst f = flatten (fmap f lst)
+        where flatten REmpty = REmpty 
+              flatten (h :< t) = flatten h <> t
--- src/Part5/Tasks.hs
+++ src/Part5/Tasks.hs
@@ -4,29 +4,31 @@ import Util(notImplementedYet)
 
 -- Реализуйте левую свёртку
 myFoldl :: (b -> a -> b) -> b -> [a] -> b
-myFoldl = notImplementedYet
+myFoldl f acc [] = acc
+myFoldl f acc (h:t) = myFoldl f (f acc h) t
 
 -- Реализуйте правую свёртку
 myFoldr :: (a -> b -> b) -> b -> [a] -> b
-myFoldr = notImplementedYet
+myFoldr f acc [] = acc
+myFoldr f acc (h:t) = f h (myFoldr f acc t)
 
 -- Используя реализации свёрток выше, реализуйте все остальные функции в данном файле
 
 myMap :: (a -> b) -> [a] -> [b]
-myMap f = notImplementedYet
+myMap f = myFoldl (\acc -> \item -> acc ++ [f item]) []
 
 myConcatMap :: (a -> [b]) -> [a] -> [b]
-myConcatMap f = notImplementedYet
+myConcatMap f = myFoldl (\acc -> \item -> acc ++ (f item)) []
 
 myConcat :: [[a]] -> [a]
-myConcat = notImplementedYet
+myConcat = myConcatMap id
 
 myReverse :: [a] -> [a]
-myReverse = notImplementedYet
+myReverse = myFoldr (\item -> \acc -> acc ++ [item]) []
 
 myFilter :: (a -> Bool) -> [a] -> [a]
-myFilter p = notImplementedYet
+myFilter p = myFoldl (\acc -> \item -> if p item then acc ++ [item] else acc ) []
 
 myPartition :: (a -> Bool) -> [a] -> ([a], [a])
-myPartition p = notImplementedYet
+myPartition p = myFoldl (\acc -> \item -> if p item then (fst acc ++ [item], snd acc) else (fst acc, snd acc ++ [item])) ([], [])
 
